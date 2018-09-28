92c50940b9a0c2b3b5c27c54d981a71c09fba2c8
anhttse <zlegendzz@gmail.com>
2018-09-28 23:00:34 +0700

    add

---
+++ Facebook/Business/FbUser.cs
@@ -0,0 +1,9 @@
+﻿namespace Facebook.Business
+{
+    public class FbUser
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string picture { get; set; }
+    }
+}
---
+++ Facebook/Business/Group.cs
@@ -0,0 +1,10 @@
+﻿namespace Facebook.Business
+{
+    public class Group
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public int member_count { get; set; }
+        public string privacy { get; set; }
+    }
+}
